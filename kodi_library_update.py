4b0538ed433f32c467692d3e4faf0a758c5ce38e
jl <josh@joshlapham.com>
2017-05-25 09:43:41 +1000

    Update kodi_library_update.py

--- kodi_library_update.py
+++ kodi_library_update.py
@@ -1,5 +1,7 @@
 #!/usr/bin python
 
+from argparse import ArgumentParser
+from threading import Thread
 from requests import post
 from json import dumps
 import kodi_cfg as cfg
@@ -7,30 +9,40 @@ import kodi_cfg as cfg
 HEADERS = {'Content-Type': 'application/json'}
 KODI_JSON_RPC_URL = "http://" + cfg.KODI_USERNAME + ":" + cfg.KODI_PASSWORD + "@" + cfg.KODI_HOST + ":" + str(cfg.KODI_PORT) + "/jsonrpc"
 
-def do_video_library_scan(logger=None):
+# TODO: right now we're calling `print response` to ensure the API calls actually happen; don't do this. Must be to do with `threading` library.
+
+def do_video_library_scan():
     payload = {"jsonrpc": cfg.KODI_JSON_RPC_VERSION, "method": "VideoLibrary.Scan", "id": "mybash"}
     response = post(KODI_JSON_RPC_URL, data=dumps(payload), headers=HEADERS)
-    
-    if logger:
-        logger.info(response)
-    
+    print response
     return response
-        
-def do_video_library_clean(logger=None):
+    
+def do_video_library_clean():
     payload = {"jsonrpc": cfg.KODI_JSON_RPC_VERSION, "method": "VideoLibrary.Clean", "id": "mybash"}
     response = post(KODI_JSON_RPC_URL, data=dumps(payload), headers=HEADERS)
-    
-    if logger:
-        logger.info(response)
-        
+    print response
     return response
     
 if __name__ == '__main__':
-    """ Makes an API call to Kodi Media Center to clean and update the video library. """
+    """ Makes an API call to Kodi Media Center to update/clean the video library. """
+    
+    args = ArgumentParser()
+    args.add_argument("--clean", help="Make API call to clean the video library", required=False, action="store_true")
+    args = args.parse_args()
     
     try:
-        do_video_library_scan()
-        do_video_library_clean()
+        # TODO: `response` variables don't do anything here
         
+        update_thread = Thread(target=do_video_library_scan)
+        update_thread.start()
+        update_response = update_thread.join()
+        print update_response
+        
+        if args.clean is True:
+            clean_thread = Thread(target=do_video_library_clean)
+            clean_thread.start()
+            clean_response = clean_thread.join()
+            print clean_response
+            
     except Exception as e:
         print "Error : %s" % e
