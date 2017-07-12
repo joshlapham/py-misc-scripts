5b19bcd8ca767d16784de54b8875a2aec07c8887
Josh Lapham <josh@joshlapham.com>
2017-07-13 07:58:36 +1000

    Update to use argparse.

--- ip_camera_record.py
+++ ip_camera_record.py
@@ -3,11 +3,13 @@
 import subprocess
 import time
 import sys
-from logger import Logger
+import logger
+import argparse
 
 FFMPEG_EXE = '/usr/local/bin/ffmpeg'
 
-LOGGER = Logger()
+# TODO: use DI for logger
+LOGGER = logger.Logger()
 
 def _do_timelapse(file, output_file):
     # TODO: form CLI string to turn a video file into a timelapse video
@@ -24,44 +26,51 @@ def _do_join(date):
     # TODO: form CLI string for list of files
     pass
 
-def _do_record():
-    output_filepath = time.strftime("%Y%m%d-%H%M%S")
+def _do_record(stream_url, output_path):
+    filename = "{}.mp4".format(time.strftime("%Y%m%d-%H%M%S"))
 
-    LOGGER.info('Recording to file %s.mp4' % output_filepath)
+    LOGGER.info('Recording to file {}'.format(filename))
 
-    # TODO: take params to this method; don't hardcode values
     CLI = [
-        '%s' % FFMPEG_EXE,
-        '-i', 'rtsp://192.168.1.38:554/onvif1',
+        '{}'.format(FFMPEG_EXE),
+        '-i', '{}'.format(stream_url),
         '-r', '30',
         '-vcodec', 'copy',
         '-an',
         '-t', '900',
-        '/Volumes/Media/ip_camera/%s.mp4' % output_filepath
+        '{}/{}'.format(output_path, filename)
     ]
 
     try:
         p = subprocess.Popen(CLI, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         p.communicate()
 
-        LOGGER.info('Finished recording to file %s.mp4' % output_filepath)
+        LOGGER.info('Finished recording to file {}'.format(filename))
 
     except subprocess.CalledProcessError as e:
         LOGGER.info(e)
 
     except Exception as e:
+        # TODO: might just need to call `raise` without the `e`?
         raise Exception(e)
 
 if __name__ == "__main__":
-    # TODO: accept command-line args
-    # TODO: write doc header for this script
+    """ Records RTSP stream from an IP camera using ffmpeg. """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--stream-url', help='RTSP stream URL', type=str, required=True)
+    parser.add_argument('--output-path', help='Path for recorded output', type=str, required=True)
+    args = parser.parse_args()
+
+    # TODO: remove
+    # rtsp://192.168.1.38:554/onvif1
+    # /Volumes/Media/ip_camera
 
     try:
         _do_record()
 
     except KeyboardInterrupt:
-        LOGGER.info('User aborted script execution')
-        sys.exit(1)
+        sys.exit('User aborted script execution')
 
     except Exception as e:
-        LOGGER.info('Exception: %s' % e)
+        LOGGER.info('Exception: {}'.format(e))
