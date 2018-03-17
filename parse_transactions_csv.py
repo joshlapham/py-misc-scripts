ec40e377303544ba3fc106fe1ef3f8a00b1b32a8
Josh Lapham <josh@joshlapham.com>
2018-03-18 09:30:22 +1000

    Add parse_transactions_csv.py

---
+++ parse_transactions_csv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import csv
+import argparse
+
+def parse_csv_file(csv_file, transaction_name):
+    """ Parses a CSV file of transactions for a given transaction name. """
+    pass
+
+if __name__ == '__main__':
+    pass
