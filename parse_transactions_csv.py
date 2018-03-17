e3653f43d75e271cda6061dc0ef9b6c8ea20ad85
Josh Lapham <josh@joshlapham.com>
2018-03-18 09:34:28 +1000

    Continue implementing parse transactions CSV script

--- parse_transactions_csv.py
+++ parse_transactions_csv.py
@@ -5,7 +5,20 @@ import argparse
 
 def parse_csv_file(csv_file, transaction_name):
     """ Parses a CSV file of transactions for a given transaction name. """
-    pass
-
+    
+    reader = csv.reader(csv_file, delimiter=' ', quotechar='|')
+    
+    for row in reader:
+        print(', '.join(row))
+        
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--csv-filepath', help='Filepath to transactions CSV file', required=True)
+    args = parser.parse_args()
+    
+    try:
+        with open(args.csv_filepath, newline='') as csv_file:
+            parse_csv_file(csv_file)
+            
+    except Exception as e:
+        print(e)
