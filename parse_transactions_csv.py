917b1da3e9506ed589b501d80c713001c7f7cc7b
Josh Lapham <josh@joshlapham.com>
2018-03-18 10:40:31 +1000

    Continue implementing parse transactions CSV script

--- parse_transactions_csv.py
+++ parse_transactions_csv.py
@@ -13,8 +13,8 @@ def calculate_total(amounts):
         
     return total
 
-def parse_csv_file(csv_file, transaction_name):
-    """ Parses a CSV file of transactions for a given transaction name. """
+def parse_csv_file(csv_file, transaction_name, transaction_month):
+    """ Parses a CSV file of transactions for a given transaction name, and month it occured. """
     
     # TODO: parse date ranges
     
@@ -26,10 +26,23 @@ def parse_csv_file(csv_file, transaction_name):
         if transaction_name is not None:
             # Parse `row` for `transaction_name` value
             if transaction_name in row[1]:
-                print('Transaction: {}'.format(row))
-                print('Amount: {}'.format(row[-2]))
-                amounts.append(row[-2])
-            
+                
+                if transaction_month is not None:
+                    # Parsing a `transaction_month`, only add amount if month matches
+                    if transaction_month in row[0].split('/')[1]:
+                        print('Matched month')
+                        # print(row[0].split('/'))
+                        # return
+                    
+                        print('Transaction: {}'.format(row))
+                        print('Amount: {}'.format(row[-2]))
+                        amounts.append(row[-2])
+                else:
+                    # No `transaction_month` to parse
+                    print('Transaction: {}'.format(row))
+                    print('Amount: {}'.format(row[-2]))
+                    amounts.append(row[-2])
+                            
     print('Total amounts: {}'.format(amounts))
     
     total = calculate_total(amounts)
@@ -40,11 +53,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--csv-filepath', help='Filepath to transactions CSV file', required=True)
     parser.add_argument('--transaction-name', help='Name of transaction to look for in CSV file', required=False, default=None, type=str)
+    parser.add_argument('--transaction-month', help='Parse a specific month that transaction occured', required=False, default=None)
     args = parser.parse_args()
     
     try:
         with open(args.csv_filepath, 'r') as csv_file:
-            parse_csv_file(csv_file, args.transaction_name)
+            parse_csv_file(csv_file, args.transaction_name, args.transaction_month)
             
     except Exception as e:
         print(e)
